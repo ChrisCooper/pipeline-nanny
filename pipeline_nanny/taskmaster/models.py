f37fa184149b2950eca28bb8661b32170d8c095c
Chris Cooper <chriscooper1991@gmail.com>
2014-07-08 03:45:23 -0400

    added a test for self-dependencies

--- pipeline_nanny/taskmaster/models.py
+++ pipeline_nanny/taskmaster/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 class JobGroup(models.Model):
 	name = models.TextField()
-	date_Created = models.DateTimeField('date created')
+	date_Created = models.DateTimeField('date created', auto_now_add=True)
+
+	def new_job(self, **args):
+		return Job.objects.create(group=self, **args)
 
 	def __repr__(self):
 		return "Job group: (" + ", ".join(self.jobs) + ")"
 
-class Job(object):
+class Job(models.Model):
 	name = models.TextField()
 	group = models.ForeignKey('JobGroup')
 	child_jobs = models.ManyToManyField('self', symmetrical=False, related_name='parent_jobs')
@@ -17,21 +20,21 @@ class Job(object):
 	
 	def add_child(self, dependant_job):
 		if dependant_job == self:
-			raise Exception("Error: Can't add a job as its own child. Job is {0}".format(self))
+			raise InvalidDependencyException("Error: Can't add a job as its own child. Job is {0}".format(self))
 		if self.depends_on(dependant_job):
-			raise Exception("Error: Dependency loops are not allowed. {0} already depends on {1}".format(self, dependant_job))
-		if dependant_job in self.child_jobs:
-			raise Exception("Error: Child job has already been added. {0} already depends on {1}".format(dependant_job, self))
+			raise InvalidDependencyException("Error: Dependency loops are not allowed. {0} already depends on {1}".format(self, dependant_job))
+		if dependant_job in self.child_jobs.all():
+			raise InvalidDependencyException("Error: Child job has already been added. {0} already depends on {1}".format(dependant_job, self))
 	   
 		self.child_jobs.add(dependant_job)
 
 	def add_parent(self, prerequisite_job):
 		if prerequisite_job == self:
-			raise Exception("Error: Can't add a job as its own parent. Job is {0}".format(self))
+			raise InvalidDependencyException("Error: Can't add a job as its own parent. Job is {0}".format(self))
 		if prerequisite_job.depends_on(self):
-			raise Exception("Error: Dependency loops are not allowed. {0} already depends on {1}".format(prerequisite_job, self))
-		if prerequisite_job in self.parent_jobs:
-			raise Exception("Error: Parent job has already been added. {0} already depends on {1}".format(self, prerequisite_job))
+			raise InvalidDependencyException("Error: Dependency loops are not allowed. {0} already depends on {1}".format(prerequisite_job, self))
+		if prerequisite_job in self.parent_jobs.all():
+			raise InvalidDependencyException("Error: Parent job has already been added. {0} already depends on {1}".format(self, prerequisite_job))
 	   
 		self.parent_jobs.add(prerequisite_job)
 
@@ -44,12 +47,14 @@ class Job(object):
 			self.add_child(job)
    
 	def depends_on(self, job):
-		if (job in self.parent_jobs):
+		if (job in self.parent_jobs.all()):
 			return True
 	   
-		for dependency in self.parent_jobs:
+		for dependency in self.parent_jobs.all():
 			if dependency.depends_on(job):
 				return True
 		return False
 
 
+class InvalidDependencyException(Exception):
+    pass
--- pipeline_nanny/taskmaster/tests.py
+++ pipeline_nanny/taskmaster/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from taskmaster.models import *
 
-# Create your tests here.
+class DependencyTestCase(TestCase):
+	def setUp(self):
+		self.group = JobGroup.objects.create(name="pizza maker group")
+		self.job1 = self.group.new_job(name="tony job")
+		self.job2 = self.group.new_job(name="steve job")
+
+	def test_adding_self(self):
+		"""Jobs can't be self-dependent"""
+		j = self.job1
+		with self.assertRaises(InvalidDependencyException):
+			j.add_parent(j)
+
+		with self.assertRaises(InvalidDependencyException):
+			j.add_child(j)
