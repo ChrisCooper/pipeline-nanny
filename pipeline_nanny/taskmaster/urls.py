fe5d2e184877be861ab424fdcece46c51d371a62
Chris Cooper <chriscooper1991@gmail.com>
2014-07-08 15:00:40 -0400

    Added a view for seeing job groups

---
+++ pipeline_nanny/taskmaster/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from taskmaster import views
+
+urlpatterns = [
+    url(r'^$', views.jobgroups, name='jobgroups')
+]
--- pipeline_nanny/taskmaster/views.py
+++ pipeline_nanny/taskmaster/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+from taskmaster.models import *
+
+def jobgroups(request):
+	groups = JobGroup.objects.all()
+	paginator = Paginator(groups, 25) # Show 25 job groups per page
+
+	page = request.GET.get('page')
+	try:
+		groups = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		groups = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		groups = paginator.page(paginator.num_pages)
+
+	return render(request, 'taskmaster/job_groups.html', {"groups": groups})
