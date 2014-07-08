00100e5919c4db773ffa5a110c7586b7bb5a81a2
Chris Cooper <chriscooper1991@gmail.com>
2014-07-08 02:32:54 -0400

    added required files for django docker

---
+++ pipeline_nanny/pipeline_nanny/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for pipeline_nanny project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pipeline_nanny.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
