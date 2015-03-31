aa1c1a5b140a72b6fa96b22099505c46ae7266d5
Chris Cooper <chris@quixotic-chris.com>
2015-03-31 17:19:27 -0400

    removed a level of nesting

--- pipeline_nanny/urls.py
+++ pipeline_nanny/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^', include('taskmaster.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
